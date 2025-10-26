1f38ebce30b4f01ccd87277300df3a02556b2cbe
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-10-27 00:45:53 +0900

    feat: 자동차 경주 실행 흐름 연결

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,11 +1,20 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = Console.readLine();
+
+        String[] names = carName.split(",");
+        List<Car> cars = new ArrayList<>();
+        for(int i=0; i<names.length; i++) {
+            String name = names[i].trim();
+            cars.add(new Car(name));
+        }
 
         System.out.println("시도할 횟수는 몇 회인가요?");
         int attempt = Integer.parseInt(Console.readLine());
@@ -13,7 +22,10 @@ public class Application {
         System.out.println();
         System.out.println("실행 결과");
 
-        System.out.println();
-        System.out.print("최종 우승자 : ");
+        RacingGame game = new RacingGame(cars, attempt);
+        game.run();
+        List<String> winners = game.getWinners();
+
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
